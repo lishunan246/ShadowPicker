bd899cc31b9db2fd135a834d987b74ae2c69b495
Li Shunan <lishunan@bilibili.com>
2017-07-05 20:10:59 +0800

    fix

--- shadowpicker.py
+++ shadowpicker.py
@@ -6,6 +6,8 @@ import re
 
 ss_program_name = "Shadowsocks.exe"
 ss_config_name = "gui-config.json"
+default_codec = "gbk"
+ping_times = 10
 
 
 def get_process():
@@ -31,35 +33,32 @@ def main():
 
     d, _ = os.path.split(cmd)
     conf_file = os.path.join(d, ss_config_name)
-    server_list = []
-    current_index = -1
+
     with open(conf_file) as f:
         conf = json.load(f)
         server_list = [x['server'] for x in conf['configs']]
         current_index = conf['index']
-        p_list = []
-        for server in server_list:
-            p = subprocess.Popen(["ping", server], stdout=subprocess.PIPE)
-            p_list.append(p)
 
-        tuples = []  # drop rate, response time, index, server name
-        i = 0
-        for p in p_list:
-            p.wait()
-            out = p.stdout.read().decode("gbk").splitlines()
-            avg_ms = re.findall(r"(\d+)(?=ms)", out[-1])
-            drop_l = re.findall(r"(\d+)", out[-3])
+    p_list = []
+    for server in server_list:
+        p = subprocess.Popen(["ping", server, "-n", str(ping_times)], stdout=subprocess.PIPE)
+        p_list.append(p)
 
-            tuples.append((int(drop_l[-1]), int(avg_ms[-1]), i, server_list[i]))
-            i += 1
+    tuples = []  # drop rate, response time, index, server name
+    i = 0
+    for p in p_list:
+        p.wait()
+        out = p.stdout.read().decode(default_codec).splitlines()
+        avg_ms = re.findall(r"(\d+)(?=ms)", out[-1])
+        drop_l = re.findall(r"(\d+)", out[-3])
 
-        ts = sorted(tuples)
-        if ts[0][2] != current_index:
-            terminate_ss()
-            update_config()
-            start_ss()
+        tuples.append((int(drop_l[-1]), int(avg_ms[-1]), i, server_list[i]))
+        i += 1
 
-        pass
+    ts = sorted(tuples)
+    better_index = ts[0][2]
+    if better_index != current_index:
+        print("server {} may be better, you are using {}".format(server_list[better_index], server_list[current_index]))
 
 
 if __name__ == '__main__':
